9c39fc2ac86313a00b0894746f547ee16d004c94
uehara0822 <166109323+uehara0822@users.noreply.github.com>
2024-04-23 16:18:34 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,15 @@
+$('#change-color').on('click',function(){
+  $('#target').addClass('red');
+});
+
+$('#change-text').on('click',function(){
+  $('#target').text('hello');
+});
+
+$('#fade-out').on('click',function(){
+
+  $('#target').fadeOut();
+});
+$('#fade-in').on('click',function(){
+  $('#target').fadeIn();
+});
